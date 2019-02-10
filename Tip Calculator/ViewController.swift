703459607113b74bdb02da8a0086af448ae5928d
Jining song <jiningsmail@gmail.com>
2019-02-10 14:07:09 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Jining song on 2019/2/10.
+//  Copyright © 2019 Jining song. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
